c434592dcf153e615528ff82ec4f20fded7200ee
Andrew Ho <ho@groupon.com>
2014-08-22 17:37:00 -0700

    Refactor to use functions from winutils module

--- movewin.c
+++ movewin.c
@@ -3,134 +3,53 @@
 #define ME "movewin"
 #define USAGE "usage: " ME " title x y [width height]\n"
 
+/* Hold target position, optional size, mutex so we only move first window */
 typedef struct {
-    int x;
-    int y;
-    int width;
-    int height;
-} WindowGeometry;
+    CGPoint position;    /* move window to this position */
+    CGSize size;         /* resize window to this size */
+    int hasSize;         /* only resize if this is true */
+    int movedWindow;     /* set to true if we have moved any window */
+} MoveWinCtx;
 
-WindowGeometry WGCreate(int x, int y, int width, int height) {
-    WindowGeometry geo;
-
-    geo.x = x;
-    geo.y = y;
-    geo.width = width;
-    geo.height = height;
-
-    return geo;
-}
-
-bool WGEqual(WindowGeometry geo1, WindowGeometry geo2) {
-    return
-        geo1.x == geo2.x &&
-        geo1.y == geo2.y &&
-        geo1.width == geo2.width &&
-        geo1.height == geo2.height;
-}
-
+/* Return true if and only if we are authorized to call accessibility APIs */
 static bool isAuthorized() {
+    /* TODO: silence deprecation warning in Mavericks and later */
     return AXAPIEnabled() || AXIsProcessTrusted();
 }
 
-WindowGeometry CFDictionaryGetBounds(CFDictionaryRef theDict) {
-    WindowGeometry geo;
+/* Callback for windowList() moves the first window it encounters */
+void moveWindow(CFDictionaryRef window, void *ctxPtr) {
+    MoveWinCtx *ctx = (MoveWinCtx *)ctxPtr;
+    AXUIElementRef appWindow = NULL;
+    CGPoint actualPosition;
+    CGPoint actualSize;
 
-    geo.x = CFDictionaryGetInt(theDict, CFSTR("X"));
-    geo.y = CFDictionaryGetInt(theDict, CFSTR("Y"));
-    geo.width = CFDictionaryGetInt(theDict, CFSTR("Width"));
-    geo.height = CFDictionaryGetInt(theDict, CFSTR("Height"));
+    /* If we already moved a window, skip all subsequent ones */
+    if(ctx->movedWindow) return;
 
-    return geo;
-}
-
-WindowGeometry AXWindowGetBounds(AXUIElementRef window) {
-    AXValueRef attrValue;
-    CGPoint windowPosition;
-    CGSize windowSize;
-    WindowGeometry geo;
-
-    AXUIElementCopyAttributeValue(
-        window, kAXPositionAttribute, (CFTypeRef *)&attrValue
-    );
-    AXValueGetValue(attrValue, kAXValueCGPointType, &windowPosition);
-    CFRelease(attrValue);
-    geo.x = windowPosition.x;
-    geo.y = windowPosition.y;
-
-    AXUIElementCopyAttributeValue(
-        window, kAXSizeAttribute, (CFTypeRef *)&attrValue
-    );
-    AXValueGetValue(attrValue, kAXValueCGSizeType, &windowSize);
-    CFRelease(attrValue);
-    geo.width = windowSize.width;
-    geo.height = windowSize.height;
-
-    return geo;
-}
-
-void AXWindowSetBounds(AXUIElementRef window, WindowGeometry geo) {
-    CGPoint windowPosition;
-    AXValueRef attrValue;
-    CGSize windowSize;
-
-    windowPosition.x = geo.x;
-    windowPosition.y = geo.y;
-    attrValue = AXValueCreate(kAXValueCGPointType, &windowPosition);
-    AXUIElementSetAttributeValue(window, kAXPositionAttribute, attrValue);
-    CFRelease(attrValue);
-
-    if(geo.width != -1 && geo.height != -1) {
-        windowSize.width = geo.width;
-        windowSize.height = geo.height;
-        attrValue = AXValueCreate(kAXValueCGPointType, &windowSize);
-        AXUIElementSetAttributeValue(window, kAXPositionAttribute, attrValue);
-        CFRelease(attrValue);
+    /* Move window, unless positions already match */
+    actualPosition = CGWindowGetPosition(window);
+    if(!CGPointEqualToPoint(ctx->position, CGWindowGetPosition(window))) {
+        appWindow = AXWindowFromCGWindow(window);
+        AXWindowSetPosition(appWindow, ctx->position);
     }
-}
 
-void moveWindow(CFDictionaryRef window, WindowGeometry newGeo) {
-    WindowGeometry geo, appWindowGeo;
-    pid_t pid;
-    AXUIElementRef app, appWindow;
-    CFArrayRef appWindowList;
-    int i;
-    CFStringRef windowTitle;
-
-    /* If new coordinates and size match, then there is nothing to do */
-    geo = CFDictionaryGetBounds(CFDictionaryGetValue(window, kCGWindowBounds));
-    if(WGEqual(geo, newGeo)) return;
-
-    /* Otherwise, load accessibility application from this PID */
-    pid = CFDictionaryGetInt(window, kCGWindowOwnerPID);
-    app = AXUIElementCreateApplication(pid);
-    AXUIElementCopyAttributeValue(
-        app, kAXWindowsAttribute, (CFTypeRef *)&appWindowList
-    );
-
-    /* Search application windows for first matching title, position, size */
-    for(i = 0; i < CFArrayGetCount(appWindowList); i++) {
-        appWindow = CFArrayGetValueAtIndex(appWindowList, i);
-        AXUIElementCopyAttributeValue(
-            appWindow, kAXTitleAttribute, (CFTypeRef *)&windowTitle
-        );
-        /* TODO: check that title matches */
-
-        appWindowGeo = AXWindowGetBounds(appWindow);
-        if(!WGEqual(geo, appWindowGeo)) continue;
-
-        AXWindowSetBounds(appWindow, newGeo);
-
-        break;
+    /* If size was specified, resize window, unless sizes already match */
+    if(ctx->hasSize) {
+        actualSize = CGWindowGetPosition(window);
+        if(!CGSizeEqualToSize(ctx->size, CGWindowGetSize(window))) {
+            if(!appWindow) appWindow = AXWindowFromCGWindow(window);
+            AXWindowSetPosition(appWindow, ctx->position);
+        }
     }
+
+    /* Record that we moved a window, so we will skip all subsequent ones */
+    ctx->movedWindow = 1;
 }
 
 int main(int argc, char **argv) {
-    char *find_title, *app_name, *window_name, *title;
-    WindowGeometry newGeo;
-    int found, i, layer, titleSize;
-    CFArrayRef windowList;
-    CFDictionaryRef window;
+    char *pattern;
+    MoveWinCtx ctx;
 
 #define WARN(msg) { fprintf(stderr, ME ": " msg "\n"); }
 #define DIE(msg) { fprintf(stderr, ME ": " msg "\n"); exit(1); }
@@ -140,54 +59,31 @@ int main(int argc, char **argv) {
     if(argc < 2) DIE_USAGE("missing required window title");
     if(argc < 4) DIE_USAGE("missing required window x and y coordinates");
     if(argc == 5) DIE_USAGE("height is required if width is present");
-    find_title = argv[1];
-    if(!find_title || !*find_title) DIE_USAGE("missing required title");
-    newGeo.x = atoi(argv[2]);
-    newGeo.y = atoi(argv[3]);
+    pattern = argv[1];
+    if(!pattern || !*pattern) DIE_USAGE("missing required title");
+    ctx.position.x = atoi(argv[2]);
+    ctx.position.y = atoi(argv[3]);
     if(argc > 5) {
-        newGeo.width = atoi(argv[4]);
-        newGeo.height = atoi(argv[5]);
-        if(newGeo.width <= 0) DIE("width must be positive integer");
-        if(newGeo.height <= 0) DIE("height must be positive integer");
+        ctx.size.width = atoi(argv[4]);
+        ctx.size.height = atoi(argv[5]);
+        if(ctx.size.width <= 0) DIE("width must be positive integer");
+        if(ctx.size.height <= 0) DIE("height must be positive integer");
+        ctx.hasSize = 1;
     } else {
-        newGeo.width = newGeo.height = -1;
+        ctx.size.width = ctx.size.height = 0;
+        ctx.hasSize = 0;
     }
     if(argc > 6) WARN("ignoring extraneous arguments");
 
     /* Die if we are not authorized to use OS X accessibility */
     if(!isAuthorized()) DIE("not authorized to use accessibility API");
 
-    windowList = CGWindowListCopyWindowInfo(
-        (kCGWindowListOptionOnScreenOnly|kCGWindowListExcludeDesktopElements),
-        kCGNullWindowID
-    );
+    /* Try to move a window */
+    ctx.movedWindow = 0;
+    windowList(pattern, moveWindow, (void *)&ctx);
 
-    found = 0;
-    for(i = 0; i < CFArrayGetCount(windowList); i++) {
-        window = CFArrayGetValueAtIndex(windowList, i);
-        layer = CFDictionaryGetInt(window, kCGWindowLayer);
-        if(layer > 0) continue;
-
-        app_name = CFDictionaryCopyCString(window, kCGWindowOwnerName);
-        window_name = CFDictionaryCopyCString(window, kCGWindowName);
-        titleSize = strlen(app_name) + strlen(" - ") + strlen(window_name) + 1;
-        title = (char *)malloc(titleSize);
-        snprintf(title, titleSize, "%s - %s", app_name, window_name);
-
-        if(fnmatch(find_title, title, 0) == 0) {
-            moveWindow(window, newGeo);
-            found = 1;
-        }
-
-        free(title);
-        free(window_name);
-        free(app_name);
-
-        if(found) break;
-    }
-    if(!found) WARN("no window matching title");
-
-    return found == 1 ? 0 : 1;
+    /* Return success if we moved any window, failure otherwise */
+    return ctx.movedWindow ? 0 : 1;
 
 #undef DIE_USAGE
 #undef DIE
