55186b8091612d3e1d4eee6926e3dfa7a932b724
Andrew Ho <ho@groupon.com>
2014-08-26 19:18:15 -0700

    Allow positions with double minus signs, to move windows relative to lower right corner

--- movewin.c
+++ movewin.c
@@ -5,11 +5,25 @@
 
 /* Hold target position, optional size, mutex so we only move first window */
 typedef struct {
+    int fromRight;       /* x coordinate is offset from right, not left */
+    int fromBottom;      /* y coordinate is offset from bottom, not top */
     CGPoint position;    /* move window to this position */
     CGSize size;         /* resize window to this size */
     int hasSize;         /* only resize if this is true */
     int movedWindow;     /* set to true if we have moved any window */
 } MoveWinCtx;
+
+/* Return true if string starts with two minus signs, and remove them */
+static bool startsWithDoubleMinus(char *s) {
+    char *p = s;
+    while(*p && isspace((unsigned char)*p)) p++;  /* skip leading whitespace */
+    if(*p == '-' && *(p + 1) == '-') {
+        *p = *(p + 1) = ' ';
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 /* Return true if and only if we are authorized to call accessibility APIs */
 static bool isAuthorized() {
@@ -24,24 +38,39 @@ static bool isAuthorized() {
 /* Callback for EnumerateWindows() moves the first window it encounters */
 void MoveWindow(CFDictionaryRef window, void *ctxPtr) {
     MoveWinCtx *ctx = (MoveWinCtx *)ctxPtr;
-    AXUIElementRef appWindow = NULL;
-    CGPoint actualPosition;
+    CGPoint newPosition, actualPosition;
     CGSize actualSize;
+    CGRect displayBounds;
+    AXUIElementRef appWindow = NULL;
     int minIdx;
 
     /* If we already moved a window, skip all subsequent ones */
     if(ctx->movedWindow) return;
 
+    /* Recalculate target window position if we got negative values */
+    newPosition = ctx->position;
+    actualSize = CGWindowGetSize(window);
+    if(ctx->fromRight || ctx->fromBottom) {
+        displayBounds = CGDisplayBounds(CGMainDisplayID());
+        if(ctx->fromRight) {
+            newPosition.x = CGRectGetMaxX(displayBounds) -
+                (actualSize.width + abs(ctx->position.x));
+        }
+        if(ctx->fromBottom) {
+            newPosition.y = CGRectGetMaxY(displayBounds) -
+                (actualSize.height + abs(ctx->position.y));
+        }
+    }
+
     /* Move window, unless positions already match */
     actualPosition = CGWindowGetPosition(window);
-    if(!CGPointEqualToPoint(ctx->position, actualPosition)) {
+    if(!CGPointEqualToPoint(newPosition, actualPosition)) {
         if(!appWindow) appWindow = AXWindowFromCGWindow(window, minIdx = 0);
-        AXWindowSetPosition(appWindow, ctx->position);
+        AXWindowSetPosition(appWindow, newPosition);
     }
 
     /* If size was specified, resize window, unless sizes already match */
     if(ctx->hasSize) {
-        actualSize = CGWindowGetSize(window);
         if(!CGSizeEqualToSize(ctx->size, actualSize)) {
             if(!appWindow) appWindow = AXWindowFromCGWindow(window, minIdx = 0);
             AXWindowSetSize(appWindow, ctx->size);
@@ -66,6 +95,8 @@ int main(int argc, char **argv) {
     if(argc == 5) DIE_USAGE("height is required if width is present");
     pattern = argv[1];
     if(!pattern || !*pattern) DIE_USAGE("missing required title");
+    ctx.fromRight = startsWithDoubleMinus(argv[2]);
+    ctx.fromBottom = startsWithDoubleMinus(argv[3]);
     ctx.position.x = atoi(argv[2]);
     ctx.position.y = atoi(argv[3]);
     if(argc > 5) {
